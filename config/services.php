271fdb2a9e17713a92c4e7de7464bebe727d84a3
tdomy <y.tokudomi@tdomy.com>
2020-08-17 11:01:26 +0000

    add socialite

--- config/services.php
+++ config/services.php
@@ -30,4 +30,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'line' => [
+        'client_id'     => env('LINE_CLIENT_ID'),
+        'client_secret' => env('LINE_CLIENT_SECRET'),
+        'redirect'      => '/login/line/callback',
+    ],
+
+    'twitter' => [
+        'client_id'     => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect'      => '/login/twitter/callback',
+    ],
+
 ];
